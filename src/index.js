268828ef6c67ef821f32b883d6daa0282569caf3
Hermes Caretta <hermescaretta@gmail.com>
2021-03-08 10:56:44 -0300

    finished, this was kinda trash tho

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import store from './store';
+import { bugAdded } from './actions';
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed', store.getState());
+});
+
+store.dispatch(bugAdded('bug 1'));
+
+unsubscribe();
+
+console.log(store.getState());
